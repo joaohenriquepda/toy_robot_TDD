f8c0efdee30b0749a699a13658cb91a95d3d34a8
João Henrique Pereira de Almeida <joaohenrique.pereira.almeida@gmail.com>
2017-01-11 15:57:57 -0200

    Add documentation using Yard-tomdoc

--- Rakefile
+++ Rakefile
@@ -1,6 +1,9 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
+require "yard"
+require "yard-tomdoc"
 
-RSpec::Core::RakeTask.new(:spec)
+YARD::Rake::YardocTask.new
+RSpec::Core::RakeTask.new :spec
 
 task :default => :spec
--- lib/toy_robotTDD/robot.rb
+++ lib/toy_robotTDD/robot.rb
@@ -1,10 +1,30 @@
 module ToyRobotTDD
+  # Public: Used to inform the client that the robot need to be placed before
+  # react to other (existent) commands.
   class NeedToBeInPlace < StandardError;
   end
 
-
+  # Public: Represents the `Toy Robot` that will actualy move throghout the
+  # table. It knows how to execute all the specified commands.
+  #
+  # Examples
+  #
+  #   robot = Robot.new
+  #   robot.place 2, 2, :north
+  #   robot.move
+  #   robot.report
   class Robot
-
+       # Public: Used to place the robot on the table in the given position.
+       #
+       # x - An Integer representing the x coordinate.
+       # y - An Integer representing the y coordinate.
+       # facing - A Symbol with the direction the robot is facing.
+       #
+       # Examples
+       #
+       #   robot.place 2, 2, :north
+       #
+       # Returns nothing.
     def place(x,y,facing)
       @x = x
       @y = y
@@ -12,6 +32,7 @@ module ToyRobotTDD
       @placed = true
     end
 
+  # Public: Used to show the client where the Toy Robot is right now.
     def report
       "#{@x},#{@y},#{@facing.to_s.upcase}"
     end
@@ -31,6 +52,16 @@ module ToyRobotTDD
 
     def right
       raise NeedToBeInPlace.new unless @placed
+
+      if @facing == :north
+        @facing = :east
+      elsif @facing == :east
+        @facing = :south
+      elsif @facing == :south
+        @facing = :west
+      elsif @facing == :west
+        @facing = :north
+      end
     end
 
     def move
@@ -45,7 +76,6 @@ module ToyRobotTDD
         @x -= 1
       end
     end
-
 
   end
 end
